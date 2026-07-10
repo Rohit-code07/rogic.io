e753b2bfdcec0599f233228bfe68d8cb9b76ccfd
ysndy <ysndy@naver.com>
2026-07-10 20:26:17 +0900

    test: update configuration test to reflect new connection pool size

--- backend/src/test/java/com/devdoyen/nemologic/LocalProfileConfigurationTest.java
+++ backend/src/test/java/com/devdoyen/nemologic/LocalProfileConfigurationTest.java
@@ -110,6 +110,6 @@ public class LocalProfileConfigurationTest {
         assertEquals("org.postgresql.Driver", driver);
 
         String maxPool = environment.getProperty("spring.datasource.hikari.maximum-pool-size");
-        assertEquals("4", maxPool);
+        assertEquals("15", maxPool);
     }
 }
